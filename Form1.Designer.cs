e22d99943ed7902f88cdcdbe0918262513e0cfd2
jodo993 <josephdo993@yahoo.com>
2018-02-20 22:28:03 -0800

    1.0 - Login Form Design + Code
    - made the design for login page
    - code written for login page functionality

---
+++ Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace Webber_Golden_Paw_Award
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.signupLabel = new System.Windows.Forms.Label();
+            this.loginPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.signupPanel = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.loginPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.signupPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // signupLabel
+            // 
+            this.signupLabel.AutoSize = true;
+            this.signupLabel.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signupLabel.Location = new System.Drawing.Point(410, 20);
+            this.signupLabel.Name = "signupLabel";
+            this.signupLabel.Size = new System.Drawing.Size(54, 16);
+            this.signupLabel.TabIndex = 1;
+            this.signupLabel.Text = "Sign Up";
+            this.signupLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.signupLabel.Click += new System.EventHandler(this.signupLabel_Click);
+            this.signupLabel.MouseLeave += new System.EventHandler(this.signupLabel_MouseLeave);
+            this.signupLabel.MouseHover += new System.EventHandler(this.signupLabel_MouseHover);
+            // 
+            // loginPanel
+            // 
+            this.loginPanel.Controls.Add(this.button1);
+            this.loginPanel.Controls.Add(this.label6);
+            this.loginPanel.Controls.Add(this.loginButton);
+            this.loginPanel.Controls.Add(this.pictureBox2);
+            this.loginPanel.Controls.Add(this.usernameTextBox);
+            this.loginPanel.Controls.Add(this.label5);
+            this.loginPanel.Controls.Add(this.label4);
+            this.loginPanel.Location = new System.Drawing.Point(300, 50);
+            this.loginPanel.Name = "loginPanel";
+            this.loginPanel.Size = new System.Drawing.Size(500, 350);
+            this.loginPanel.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(330, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 50);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Login via Computer";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(265, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "-Or-";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.Location = new System.Drawing.Point(175, 180);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(60, 25);
+            this.loginButton.TabIndex = 8;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::Webber_Golden_Paw_Award.Properties.Resources.usernameIcon;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(210, 120);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.usernameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.usernameTextBox.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameTextBox.Location = new System.Drawing.Point(45, 130);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(160, 15);
+            this.usernameTextBox.TabIndex = 5;
+            this.usernameTextBox.Text = "Username";
+            this.usernameTextBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.usernameTextBox_MouseClick);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(40, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(200, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "________________________";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(40, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Please enter...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(400, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "/";
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginLabel.Location = new System.Drawing.Point(355, 20);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(44, 16);
+            this.loginLabel.TabIndex = 3;
+            this.loginLabel.Text = "Login";
+            this.loginLabel.Click += new System.EventHandler(this.loginLabel_Click);
+            this.loginLabel.MouseLeave += new System.EventHandler(this.loginLabel_MouseLeave);
+            this.loginLabel.MouseHover += new System.EventHandler(this.loginLabel_MouseHover);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Webber_Golden_Paw_Award.Properties.Resources.treasureMapBackground;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // signupPanel
+            // 
+            this.signupPanel.Controls.Add(this.comboBox1);
+            this.signupPanel.Controls.Add(this.button2);
+            this.signupPanel.Controls.Add(this.checkBox1);
+            this.signupPanel.Controls.Add(this.textBox2);
+            this.signupPanel.Controls.Add(this.label9);
+            this.signupPanel.Controls.Add(this.textBox1);
+            this.signupPanel.Controls.Add(this.label8);
+            this.signupPanel.Controls.Add(this.label7);
+            this.signupPanel.Location = new System.Drawing.Point(300, 47);
+            this.signupPanel.Name = "signupPanel";
+            this.signupPanel.Size = new System.Drawing.Size(500, 350);
+            this.signupPanel.TabIndex = 4;
+            this.signupPanel.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(185, 50);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(130, 20);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Please fill out...";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(155, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 15);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Full name";
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(150, 100);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(200, 20);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "________________________";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(155, 145);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(160, 15);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "Username";
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(150, 145);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(200, 20);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "________________________";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(150, 219);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(200, 24);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Link this computer to my account";
+            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(215, 249);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 25);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Sign Up!";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Kindergarten",
+            "1st Grade",
+            "2nd Grade",
+            "3rd Grade",
+            "4th Grade",
+            "5th Grade",
+            "6th Grade",
+            "Office",
+            "Student Council"});
+            this.comboBox1.Location = new System.Drawing.Point(188, 180);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(120, 24);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.Text = "Select grade";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 400);
+            this.Controls.Add(this.signupPanel);
+            this.Controls.Add(this.loginPanel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.signupLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.loginLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.loginPanel.ResumeLayout(false);
+            this.loginPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.signupPanel.ResumeLayout(false);
+            this.signupPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label signupLabel;
+        private System.Windows.Forms.Panel loginPanel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel signupPanel;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Webber_Golden_Paw_Award
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loginLabel_MouseHover(object sender, EventArgs e)
+        {
+            loginLabel.Font = new Font("Arial", 10, FontStyle.Bold);
+            loginLabel.ForeColor = Color.Red;
+        }
+
+        private void loginLabel_MouseLeave(object sender, EventArgs e)
+        {
+            if (loginPanel.Visible == false)
+                loginLabel.Font = new Font("Arial", 8, FontStyle.Regular);
+            loginLabel.ForeColor = Color.Black;
+        }
+
+        private void loginLabel_Click(object sender, EventArgs e)
+        {
+            loginPanel.Visible = true;
+            signupPanel.Visible = false;
+
+            loginLabel.Font = new Font("Arial", 10, FontStyle.Bold);
+            signupLabel.Font = new Font("Arial", 8, FontStyle.Regular);
+        }
+
+        private void signupLabel_MouseHover(object sender, EventArgs e)
+        {
+            signupLabel.Font = new Font("Arial", 10, FontStyle.Bold);
+            signupLabel.ForeColor = Color.Red;
+        }
+
+        private void signupLabel_MouseLeave(object sender, EventArgs e)
+        {
+            if (signupPanel.Visible == false)
+                signupLabel.Font = new Font("Arial", 8, FontStyle.Regular);
+            signupLabel.ForeColor = Color.Black;
+        }
+
+        private void signupLabel_Click(object sender, EventArgs e)
+        {
+            signupPanel.Visible = true;
+            loginPanel.Visible = false;
+
+            signupLabel.Font = new Font("Arial", 10, FontStyle.Bold);
+            loginLabel.Font = new Font("Arial", 8, FontStyle.Regular);
+        }
+
+        private void usernameTextBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            usernameTextBox.Clear();
+        }
+    }
+}
